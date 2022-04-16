47c0ad64224964d4356b294a0b6eeeebb73ba227
username <youremail@domain.com>
2022-04-15 22:22:08 -0400

    syntax error flight is not define

---
+++ config/database.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL);
+
+// shortcut variable to mongoose.connection
+const db = mongoose.connection;
+
+db.on('connected', function() {
+    console.log(`connected to MongoDB ${db.name} at ${db.host}:${db.port}`);
+});
---
+++ models/flight.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+// Optional shortcut to the mongoose.Schema class
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+    title: String,
+    releaseYear: Number,
+    mpaaaRating: String,
+    cast: [String],
+    nowShowing : Boolean,
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
